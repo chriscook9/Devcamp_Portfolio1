fd6cb8997a9236a008e4995c37b4dae48c346901
Chris Cook <chriscook@Chriss-MBP.ad.uvu.edu>
2017-12-01 14:33:35 -0700

    Integrated new and created aactons for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,21 @@ class PortfoliosController < ApplicationController
 	def index
 		@portfolio_items = Portfolio.all
 	end
+
+	def new
+		@portfolio_item = Portfolio.new
+	end
 end
+
+def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolio_path, notice: 'Your Portfolio was successfully created.' }
+      else
+        format.html { render :new }
+        
+      end
+    end
+  end
