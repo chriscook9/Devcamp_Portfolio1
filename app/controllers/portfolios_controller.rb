edb4a2ac5dab772d52b0e5858e8adea40a8a2a7c
Chris Cook <chriscook@Chriss-MBP.ad.uvu.edu>
2017-12-05 15:53:32 -0700

    Integrated Nested atrribute for portfolio model

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -7,17 +7,17 @@ class PortfoliosController < ApplicationController
 		@portfolio_item = Portfolio.new
 	end
 
-	def create
-		@portfolio_item = portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
-		respond_to do |format|
-			if @portfolio_item.save
-				format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
-			else
-				format.html { render :new }
-			end
-		end
-	end
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
 
 
 	def edit
--- db/seeds.rb
+++ db/seeds.rb
@@ -46,3 +46,15 @@ end
 end
 
 puts "9 portfolio items created"
+
+
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+    name: "Technology #{technology}"
+  )
+end
+
+
+
+
+
