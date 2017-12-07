6db720c6414323135300a4c133226793ebd64be2
Chris Cook <chriscook@Chriss-MBP.ad.uvu.edu>
2017-12-07 14:11:55 -0700

    refactored application controller to include concern for source feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20171206171858) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +57,19 @@ ActiveRecord::Schema.define(version: 20171206171858) do
     t.text "badge"
   end
 
+  create_table "technologies", force: :cascade do |t|
+    t.string "name"
+    t.bigint "portfolio_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["portfolio_id"], name: "index_technologies_on_portfolio_id"
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -78,22 +89,6 @@ ActiveRecord::Schema.define(version: 20171206171858) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
-  create_table "technologies", force: :cascade do |t|
-    t.string "name"
-    t.bigint "portfolio_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["portfolio_id"], name: "index_technologies_on_portfolio_id"
-  end
-
-  create_table "topics", force: :cascade do |t|
-    t.string "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   add_foreign_key "blogs", "topics"
   add_foreign_key "technologies", "portfolios"
-
 end
