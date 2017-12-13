dd6e0367a2c14e32422abb4254670bdea0eac862
Chris Cook <cdani3l.cook@gmail.com>
2017-12-13 10:57:08 -0700

    styled user edit page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
-def login_helper
+def login_helper style
   if current_user.is_a?(GuestUser) 
-  	 (link_to "Register", new_user_registration_path) + 
-  	  "<br>".html_safe +
-  	  (link_to "Login", new_user_session_path)
+  	 (link_to "Register", new_user_registration_path, class: style) + 
+  	  "".html_safe +
+  	  (link_to "Login", new_user_session_path, class: style)
     else
-      link_to "Logout", destroy_user_session_path, method: :delete 
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style 
      end 
   end
 
@@ -20,6 +20,6 @@ def login_helper
  	end
   end
     def copyright_generator
-      DevcampViewTool::Renderer.copyright 'Chris cook', 'All rights reserved'
+      DevcampViewTool::Renderer.copyright 'Chris Cook', 'All rights reserved'
   end
 end
