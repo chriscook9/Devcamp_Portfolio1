4c05ec77a3b70bd8437fe19f1cea519aa2881999
Chris Cook <cdani3l.cook@gmail.com>
2017-12-14 13:30:33 -0700

    Implemented show page styles for portfolio layout

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,19 +1,19 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
-  layout 'blog'
-  access all: [:sow, :index], user: { except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
+  layout "blog"
+  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]}, site_admin: :all
 
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.all
+    @blogs = Blog.page(params[:page]).per(5)
     @page_title = "My Portfolio Blog"
   end
 
   # GET /blogs/1
   # GET /blogs/1.json
   def show
-    @page_title = @blog_title
+    @page_title = @blog.title
     @seo_keywords = @blog.body
   end
 
@@ -33,9 +33,9 @@ class BlogsController < ApplicationController
 
     respond_to do |format|
       if @blog.save
-        format.html { redirect_to @blog, notice: 'Post is now live.' }
+        format.html { redirect_to @blog, notice: 'Your post is now live.' }
       else
-        format.html { render :new } 
+        format.html { render :new }
       end
     end
   end
@@ -46,10 +46,8 @@ class BlogsController < ApplicationController
     respond_to do |format|
       if @blog.update(blog_params)
         format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
-        format.json { render :show, status: :ok, location: @blog }
       else
         format.html { render :edit }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -59,7 +57,7 @@ class BlogsController < ApplicationController
   def destroy
     @blog.destroy
     respond_to do |format|
-      format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
+      format.html { redirect_to blogs_url, notice: 'Post was removed.' }
       format.json { head :no_content }
     end
   end
@@ -69,9 +67,9 @@ class BlogsController < ApplicationController
       @blog.published!
     elsif @blog.published?
       @blog.draft!
-      
     end
-    redirect_to blogs_url notice: 'Post status has been updated.'
+        
+    redirect_to blogs_url, notice: 'Post status has been updated.'
   end
 
   private
