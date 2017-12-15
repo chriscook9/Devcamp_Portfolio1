d861a8ce6e6da1e0f4134254b52ccfddb74b347e
Chris Cook <cdani3l.cook@gmail.com>
2017-12-15 09:45:22 -0700

    Update portfolioto include attr for position

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,27 +1,3 @@
-# module ApplicationHelper
-# def login_helper style = ''
-#   if current_user.is_a?(GuestUser) 
-#   	 (link_to "Register", new_user_registration_path, class: style) + 
-#   	  "".html_safe +
-#   	  (link_to "Login", new_user_session_path, class: style)
-#     else
-#       link_to "Logout", destroy_user_session_path, method: :delete, class: style 
-#      end 
-#   end
-
-#  def sample_helper
-#  	content_tag(:div, "My content", class: "my-class")
-#   end
-
-#  def source_helper(layout_name)
-#  	if session[:source]
-#  		greeting ="Thanks for visitng me from #{session[:source]} and you are on the #{layout_name} layout"
-#  		content_tag(:p, greeting, class: "spurce-greeting")
-#  	end
-#   end
-#     def copyright_generator
-#       DevcampViewTool::Renderer.copyright 'Chris Cook', 'All rights reserved'
-#   end 
 module ApplicationHelper
   def login_helper style = ''
     if current_user.is_a?(GuestUser)
