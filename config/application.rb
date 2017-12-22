e2e67f0ef899fa01ed418f402aa1911d4b5a7279
Chris Cook <cdani3l.cook@gmail.com>
2017-12-22 11:31:20 -0700

    added file

--- config/application.rb
+++ config/application.rb
@@ -10,16 +10,15 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
-require "dotenv-rails"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module DevacampPortfolio
+module DevcampPortfolio
   class Application < Rails::Application
-    config.eager_load_paths << "#{rails.root}/lib"
-    config.secret_key_base = ENV{"SECRET_KEY_BASE"}
+    config.eager_load_paths << "#{Rails.root}/lib"
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
   end
 end
