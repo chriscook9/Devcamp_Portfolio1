abd518e7695775ca94a34146e870c83f11b0a4cf
Chris Cook <cdani3l.cook@gmail.com>
2017-12-22 13:44:40 -0700

    Integrated production calls for action cable

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,9 +13,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
+
 
 //= require jquery3
+//= require rails-ujs
 //= require jquery-ui
 //= require html.sortable
 //= require popper
