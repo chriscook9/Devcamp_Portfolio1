27a51618c9dea8c46040be2cd15bc3a786622c53
Chris Cook <cdani3l.cook@gmail.com>
2017-12-22 15:25:35 -0700

    updated redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_cable.allowed_request_origins = ['https://chris-cook.herokuapp.com/', 'http://chris-cook.herokuapp.com/']
+  config.action_cable.allowed_request_origins = ['https://chris-cook.herokuapp.com', 'http://chris-cook.herokuapp.com']
   config.action_cable.url = "wss://chris-cook.herokuapp.com/cable"
 end
